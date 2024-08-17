b4e1b0cd73ea274a822c8b2c3b3649f891415f2c
Kristina-Uryevna <mironovakristina.ur@gmail.com>
2024-08-17 17:20:58 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second == third:
+        print('Количество равных чисел: ', 3)
+elif first == second or first == third or second == third:
+        print('Количество равных чисел: ', 2)
+else :
+        print('Количество равных чисел: ', 0)
